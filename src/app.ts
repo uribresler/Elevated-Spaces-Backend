87320a7f6eb70af25d553e1cf074f5a9e18e2d29
Saifullah Ahmed <saifullahahmed380@gmail.com>
2026-01-14 16:54:24 +0500

    updated cors for testing

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ import cors from 'cors'
 
 const app = express();
 
-app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: process.env.FRONTEND_URL || 'https://elevate-spaces.vercel.app', credentials: true }));
 app.use(express.json());
 
 // Initialize Passport
